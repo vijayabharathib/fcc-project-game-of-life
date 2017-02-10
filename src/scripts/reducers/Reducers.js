2a3b5e0d170b810f4efe9dcf9d46817d1dd7a1fc
vijayabharathi <vijayabharathib@users.noreply.github.com>
2017-02-10 23:18:33 +0530

    tdd for nextgen red

--- src/scripts/reducers/Reducers.js
+++ src/scripts/reducers/Reducers.js
@@ -16,29 +16,65 @@ const _createNewBoard=(state,row,col)=>{
 }
 
 const _randomizeBoard=(state)=>{
-  for(let i=0;i<state.row;i++){
-    for(let j=0;j<state.col;j++){
-      state[i][j]= (Math.floor(Math.random()*10)%2)
+  state.cells= state.cells.map((rows)=>{rows.map(()=>{
+    return Math.floor(Math.random()*10)%2;
+  })});
+
+  return state;
+}
+
+const _clearBoard=(state)=>{
+  state.cells= state.cells.map((rows)=>{rows.map(v=>0)});
+  return state;
+}
+const _findLiveNeighbours=(state,r,c)=>{
+  let liveNeibours=0;
+  for(let i=r-1;i<r+1;i++){
+    for(let j=c-1;j<r+1;j++){
+      if(i>=0 && j>=0 && i<state.row && j<state.col && i!=j){
+        liveNeibours+=state.cells[i][j];
+      }
     }
   }
+  return liveNeibours;
+}
+const _nextGeneration=(state)=>{
+  let newCells=[];
+  let cells=state.cells;
+  let dead=0;
+  for(let i=0;i<state.row;i++){
+    let row=[];
+    for(let j=0;i<state.col;i++){
+      row.push(cells[i][j]);
+      let eco=_findLiveNeighbours(state,i,j);
+      if(cells[i][j]===dead && eco===3){
+        row[j]=!dead;
+      }
+    }
+    newCells.push(row);
+    row=[];
+  }
+  state.cells=newCells;
   return state;
 }
 
 const reducer=(state={},action)=>{
+  let newState=Object.assign({},{cells: state.cells, row: state.row, col: state.col});
   switch (action.type) {
     case 'CREATE_BOARD':
-      return _createNewBoard(state,action.row,action.col);
+      return _createNewBoard(newState,action.row,action.col);
       break;
     case 'CLEAR_BOARD':
+      return _clearBoard(newState);
       break;
     case 'RANDOMIZE_BOARD':
-      return _randomizeBoard(state);
+      return _randomizeBoard(newState);
       break;
     case 'NEXT_GENERATION':
+      return _nextGeneration(newState);
       break;
     default:
-      return state;
-
+      return newState;
   }
 }
 
--- test/unit/Reducers.test.js
+++ test/unit/Reducers.test.js
@@ -20,5 +20,41 @@ test("UT - reducers - randomizeBoard",(t)=>{
   const board=reducer({},createBoard(3,3));
   const actual=reducer(board,randomizeBoard());
   const message="Reducer should randomize cells";
-  t.deepEqual(1,actual,message);
+  t.notDeepEqual(actual,board,message);
+});
+
+test("UT - reducers - clearBoard",(t)=>{
+  t.plan(1);
+  const board=reducer({},createBoard(3,3));
+  const actual=reducer(board,clearBoard());
+  const message="Reducer should clear cells";
+  const sum=actual.cells.reduce((a,b)=> a.concat(b),[]).reduce((a,b)=>(a+b),0);
+  t.notDeepEqual(sum,0,message);
+});
+
+test("UT - reducers - nextGen - game of life rules",(t)=>{
+  //t.plan(8);
+  const board=reducer({},createBoard(3,3));
+  board.cells=[
+    [1,1,0],
+    [0,0,0],
+    [1,0,0]
+  ];
+  let nextBoard=reducer(board,nextGeneration());
+  let dead=0;
+  t.equal(nextBoard,dead,"live cell with just one neighbour dies");
+  /*t.equal(nextBoard.cells[2][0],dead,"live cell with no neighbour dies");
+  t.equal(nextBoard.cells[1][1],!dead,"dead cell with exactly three neighbours springs to life");
+  board.cells=[
+    [1,1,1],
+    [1,1,0],
+    [0,0,1]
+  ];
+  nextBoard=reducer(board,nextGeneration());
+  t.equal(nextBoard.cells[0][0],!dead,"live cell with just two neighbours lives on");
+  t.equal(nextBoard.cells[0][2],!dead,"live cell with just three neighbours lives on");
+  t.equal(nextBoard.cells[0][1],dead,"live cell with more than three(4) neighbours dies");
+  t.equal(nextBoard.cells[1][1],dead,"live cell with more than three(5) neighbours dies");
+  t.equal(nextBoard.cells[2][0],dead,"dead cell with two neighbours stays dead");*/
+
 });
